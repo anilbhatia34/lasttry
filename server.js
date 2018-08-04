5c634cfe7389a3daad06744ebda8da23e9c2cbb9
anil <anilbhatia34@gmail.com>
2018-08-05 00:59:42 +0530

    ihi

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + 'front-end/dist/'));
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist'));
+app.listen(3000);
