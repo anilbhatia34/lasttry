ac8ce483e365cdfaad245ae0fadb48b704b3ffe5
anil <anilbhatia34@gmail.com>
2018-08-05 01:09:22 +0530

    tretr

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/front-end/dist'));
 
 app.get('*', (req, res) => {
     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
